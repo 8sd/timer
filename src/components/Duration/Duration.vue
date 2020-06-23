eb5693b0790ddb97cc36c70bdb5759344e3f4f42
Sebastian Duda <git@sebdu.de>
2020-06-23 13:05:39 +0200

    remove --:-- if not needed

--- src/components/Duration/Duration.vue
+++ src/components/Duration/Duration.vue
@@ -1,7 +1,12 @@
 <template>
 <v-label>
   <v-icon v-if="icon !== ''">{{icon}}</v-icon> 
+  <span v-if="passed === null">
+    <span class="total">{{Total}}</span>
+  </span>
+  <span v-else>
   <span class="passed">{{Passed}}</span> / <span class="total">{{Total}}</span>
+  </span>
 </v-label>
 </template>
 
