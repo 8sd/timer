c039448ca7a366ccf81d10f1977042fa66332a4b
Sebastian Duda <git@sebdu.de>
2021-01-02 16:25:10 +0100

    Change icon attribution and legal info placeholder

--- src/components/Title/Title.vue
+++ src/components/Title/Title.vue
@@ -1,6 +1,6 @@
 <template>
 <div style="text-align: center">
-  <img src="/timer.svg" @click="req"/>
+  <img src="/timer.svg" @click="req" attribution="https://www.flaticon.com/authors/pixel-perfect"/>
   <h1>Timer</h1>
   <div class="animated bounceOutLeft delay-3s" id="notificationHint">
     <v-label>Press the icon to request the permissions for the notifications</v-label> 
